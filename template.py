cfdc6b380edfb8b74693713fa83e238de0706b8f
kk06112001 <kk1455217@gmail.com>
2025-03-28 18:30:43 +0530

    final commit

--- template.py
+++ template.py
@@ -2,12 +2,12 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name="datascience"
 
 list_of_files=[
-    ".github/workflows/.gitkeep",
+    ".gthub/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -22,23 +22,30 @@ list_of_files=[
     "params.yaml",
     "schema.yaml",
     "main.py",
-    "DockerFile",
+    "Dockerfile",
     "setup.py",
     "research/research.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    "app.py"
+
 ]
+
 
 for filepath in list_of_files:
     filepath=Path(filepath)
     filedir,filename=os.path.split(filepath)
+
     if filedir!="":
         os.makedirs(filedir,exist_ok=True)
-        logging.info(f"Creating directory {filedir} for the file: {filename}")
-        
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open(filepath,'w') as f:
+        logging.info(f"Creating directory {filedir} for the file : {filename}")
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+
             pass
-            logging.info(f"creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
+
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
             
+
